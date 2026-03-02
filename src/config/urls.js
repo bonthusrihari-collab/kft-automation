142b47d9069f94e4255a05f75c9e7d83b46a6cb1
Sriharibabu <bonthu.srihari@dev.knightfintech.com>
2026-03-02 22:45:23 +0530

    added some files in git ignore

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,3 +2,5 @@ export const angelOneBaseUrl = "https://openapiuat.angelone.in/openApi";
 export const kftAuthBaseUrl = 'https://angelone-auth-api-qa1.azurewebsites.net'
 export const muthootBaseUrl = 'https://mf-embed-auth-api-uat.knightfintech.com'
 export const kftBaseUrl = 'https://angelone-aurix-api-qa1.azurewebsites.net'
+
+export const embededUrlRFIn=''
--- src/tests/playwright/example.spec.js
+++ src/tests/playwright/example.spec.js
@@ -1,18 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+// Simple login test converted from standalone script
 
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
-});
+test('agent login flow', async ({ page }) => {
+  await page.goto('https://rf-embed-dsa-app.knightfintech.com/');
 
-test('get started link', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+  await page.getByRole('textbox', { name: 'Please Enter Agent ID' }).click();
+  await page.getByRole('textbox', { name: 'Please Enter Agent ID' }).fill('bonthu.srihari@dev.knightfintech.com');
+  await page.getByRole('textbox', { name: 'Please Enter Password' }).click();
+  await page.getByRole('textbox', { name: 'Please Enter Password' }).fill('welcome@1234');
+  await page.locator('i').click();
+  await page.getByRole('button', { name: 'Sign in' }).click();
 
-    // Click the get started link.
-    await page.getByRole('link', { name: 'Get started' }).click();
-
-    // Expects page to have a heading with the name of Installation.
-    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // optional assertion to verify successful login
+//   await expect(page).toHaveURL(/dashboard/);
 });
